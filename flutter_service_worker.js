e72317880664116ea8753a001ec005e3a02f8e36
Abdullah Harits <harits.abdullah19@gmail.com>
2021-12-09 16:22:22 +0700

    add medium and project

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "0cdb53182d6819492ff6815a7f3c693a",
-"index.html": "6631e2408c0d3bde726b1f419d1e8bdf",
-"/": "6631e2408c0d3bde726b1f419d1e8bdf",
-"main.dart.js": "f09d422d5bee5e4cdfe0bbc86313adf1",
+"index.html": "98ada707afc4e769570efbca55d32508",
+"/": "98ada707afc4e769570efbca55d32508",
+"main.dart.js": "a37e263f8932e1fd360a0882c759cfca",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
